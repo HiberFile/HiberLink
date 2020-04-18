093bf569687595cbf2a4a124ff3a64b9d0fdc423
Bruno Paiva <ppaiva11144@gmail.com>
2020-04-18 14:23:37 +0200

    Update footer.php
    br added

--- src/html/footer.php
+++ src/html/footer.php
@@ -14,6 +14,7 @@ if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
     <footer class="flex flex-col md:flex-row items-start w-full flex-none self-start p-6 font-medium text-xs text-grey-dark md:items-center justify-between">
         <p><a href="<?php echo env('ext_url'); ?>/mentions.php"><u>Mentions légales</u></a> | <span class="copyleft">&copy;</span> 2020 <i>HiberFile Team</i> & <u><a href="https://github.com/HiberFile/HiberLink/graphs/contributors">Contributors</a></u> | <u><a href="https://github.com/HiberFile/HiberLink/commit/<?php echo get_current_git_commit()?>"><?php echo get_current_git_commit()?></a></u></p>
         <ul class="list-reset flex flex-col md:flex-row items-start md:items-center md:justify-end" >
+            <br>
             <li><?php echo env('title'); ?> est un service de création de petits liens simple, gratuit et <a href="https://github.com/HiberFile/HiberLink" target="_blank">Open Source</a>.</li>
         </ul>
     </footer>
