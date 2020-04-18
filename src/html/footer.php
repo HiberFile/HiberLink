cd508a46a7275b87aa53d161906fd9505c518f76
Jus de Patate_ <jusdepatate@protonmail.com>
2020-04-18 14:36:52 +0200

    fix typo

--- src/html/footer.php
+++ src/html/footer.php
@@ -15,7 +15,7 @@ if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
         <p><a href="<?php echo env('ext_url'); ?>/mentions.php"><u>Mentions légales</u></a> | <span class="copyleft">&copy;</span> 2020 <i>HiberFile Team</i> | <u><a href="https://github.com/HiberFile/HiberLink/commit/<?php echo get_current_git_commit()?>"><?php echo get_current_git_commit("master", true)?></a></u></p>
         <ul class="list-reset flex flex-col md:flex-row items-start md:items-center md:justify-end" >
             <br>
-            <li><?php echo env('title'); ?> est un service de création de petits liens simple, gratuit et <a href="https://github.com/HiberFile/HiberLink" target="_blank">Open Source</a>.</li>
+            <li><?php echo env('title'); ?> est un service de création de petits liens simple, gratuit et <u><a href="https://github.com/HiberFile/HiberLink" target="_blank">open source</a></u>.</li>
         </ul>
     </footer>
 </body>
