f6ab1f83faddf50f73c148da9bf31bb5bdbd1958
Jus de Patate_ <jusdepatate@protonmail.com>
2020-04-21 16:55:13 +0200

    fix hiberlink not redirecting

--- index.php
+++ index.php
@@ -2,11 +2,10 @@
 
 require "autoload.php";
 
-add_header();
-
 $argument = preg_split('/[\/].*[?]/', $_SERVER["REQUEST_URI"]);
 if (sizeof($argument) === 2) {
     $argument = $argument[1];
+
     $dsn = "mysql:host=" . env("mysql_address") . ";dbname=" . env("mysql_databse") . ";port=".env("mysql_port").";charset=utf8mb4";
     $options = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
@@ -25,16 +24,19 @@ if (sizeof($argument) === 2) {
 
     $row = $req->fetch();
     if (isset($row['original'])) {
-        echo $row['original'];
-        header("Status: 301 Moved Permanently", false, 301);
+        header("Status: 301 Moved Permanently", true, 301);
         header("Location: ".$row['original']);
+        add_header();
+        echo $row['original'];
     } else {
+        add_header();
         ?>
         <div class="center"><h4>Ce lien n'existe pas.</h4></div>
         <a class="btn rounded-lg flex items-center mt-2" href="<?= env("ext_url") ?>">Revenir à l'accueil</a>
         <?php
     }
 } else {
+    add_header();
     ?>
                     <img src="<?= env('ext_url') ?>/src/img/add.png" width="48" alt="+">
                     <div class="center"><h4>Transformez votre lien dès maintenant.</h4></div>
