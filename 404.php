633f240eaa4df351e5fc6a3dcce49234a9317703
Jus de Patate_ <jusdepatate@protonmail.com>
2020-06-20 21:50:20 +0200

    seo
    - 404 page
    - robots.txt
    - humans.txt
    - sitemap
    - minify normalize.css
    - delete sweetalert

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php
+
+require "autoload.php";
+
+add_header();
+?>
+                    <div class="center">
+                        <h2>404</h2>
+                        <h3>Le fichier recherché n'existe pas.</h3>
+                    </div>
+<?php
+
+add_footer();
